96efd61ce417bfc3aac26575b618c5b8f8735e4d
DianaGabiFlores <diana.gabi.flores@gmail.com>
2023-11-29 22:23:49 -0600

    Registro cambios
    Sweet alert modificado.

--- php/Regis_script.php
+++ php/Regis_script.php
@@ -13,14 +13,20 @@ if(isset($_SESSION['repetido'])){
     </script><?php
 }elseif(isset($_SESSION['insertar'])){
     ?><script>
-        swal.fire({
-            icon: "success",
-            title: "Usuario creado",
-        }).then((result) => {
-            if (result.isConfirmed) {
-                location. assign('../index.php')
-            }});
-    </script><?php
+        Swal.fire({
+        icon: "success",
+        title: "Usuario creado",
+        width: 600,
+        padding: "3em",
+        color: "black",
+        background: "#fff url(../images/trees.png)",
+        backdrop: `
+            rgba(247,140,162,0.2)
+            url("imagenes/Dulces3.gif")
+            left top
+            no-repeat
+            `
+        });<?php
 }elseif(isset($_SESSION['cont'])){
     ?><script>
         swal.fire({
