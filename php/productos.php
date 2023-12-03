7c59abaa052bac9fbfb7c475fa8b115a0bdb4f5d
AdrianBalandran <adrianbala76@gmail.com>
2023-12-03 12:16:35 -0600

    Cambios de los estilos de los productos y al archivo de registros
    Cambio de estilos y se quito un start session del archivo de registro.

--- php/productos.php
+++ php/productos.php
@@ -1,86 +1,120 @@
-<?php 
+<?php
 session_start();
 
-if ($_SESSION['usuario']){?>
-    <div id="logeado" style="display:none;">true</div> 
-<?php }else{ ?>
+if ($_SESSION['usuario']) { ?>
+    <div id="logeado" style="display:none;">true</div>
+<?php } else { ?>
     <div id="logeado" style="display:none;">false</div>
     <!-- die("You must be logged in!!"); -->
 <?php }
 
-    function datos($conexion, $categorias, $num){
-        if($num == 3){
-            $sql = "SELECT *FROM productos;";
-        }else {
-            $sql = "SELECT *FROM productos WHERE Categoria= '$categorias[$num]';";
-        }
-        $resultado = $conexion -> query($sql);
-        $i = 0;  
-        while( $fila = $resultado -> fetch_assoc() ){
-          
-            $flag = true; ?>
-            <div class="Producto_in">
-                <div class="imagen">
-                    <img src="<?php echo $fila['imagen'] ?>" alt="Producto">
-                </div>
-                <h5><?php echo $fila['nombre'] ?></h5>
-                <p class="Descripcion"><?php echo $fila['descripcion'] ?></p>
-                <?php if($fila['descuento'] != 0){
-                    $PrecioD=$fila['precio'] * ((100-$fila['descuento'])/100); ?>
-                    <p class="precio">$<?php echo $PrecioD." \t "?><del class="Precio_tachado">$<?php echo $fila['precio'] ?></del></p>
-                <?php } else { ?>
-                    <p class="precio">$<?php echo $fila['precio'] ?></p>
-                <?php } ?>
-                <hr>
-                <?php if($fila['existencias'] == 0){ $flag= false;?>
-                    <h6 class="Agotado">Producto agotado</h6>
-                <?php }else { ?>
-                    <h6>Existencias: <?php echo $fila['existencias'] ?></h6>
-                <?php } 
-                ?>
-                <h6 style="display:none;" id="exis<?php echo $i ?>" ><?php echo $fila['existencias'] ?></h6>
-                <?php if($flag){ ?>
+function datos($conexion, $categorias, $num)
+{
+    if ($num == 3) {
+        $sql = "SELECT *FROM productos;";
+    } else {
+        $sql = "SELECT *FROM productos WHERE Categoria= '$categorias[$num]';";
+    }
+    $resultado = $conexion->query($sql);
+    $i = 0;
+    while ($fila = $resultado->fetch_assoc()) {
+
+        $flag = true; ?>
+        <div class="Producto_in">
+            <div class="imagen">
+                <img src="<?php echo $fila['imagen'] ?>" alt="Producto">
+            </div>
+            <h5>
+                <?php echo $fila['nombre'] ?>
+            </h5>
+            <p class="Descripcion">
+                <?php echo $fila['descripcion'] ?>
+            </p>
+            <?php if ($fila['descuento'] != 0) {
+                $PrecioD = $fila['precio'] * ((100 - $fila['descuento']) / 100); ?>
+                <p class="precio">$
+                    <?php echo $PrecioD . " \t " ?><del class="Precio_tachado">$
+                        <?php echo $fila['precio'] ?>
+                    </del>
+                </p>
+            <?php } else { ?>
+                <p class="precio">$
+                    <?php echo $fila['precio'] ?>
+                </p>
+            <?php } ?>
+            <hr>
+            <?php if ($fila['existencias'] == 0) {
+                $flag = false; ?>
+                <h6 class="Agotado">Producto agotado</h6>
+            <?php } else { ?>
+                <h6>Existencias:
+                    <?php echo $fila['existencias'] ?>
+                </h6>
+            <?php }
+            ?>
+            <h6 style="display:none;" id="exis<?php echo $i ?>">
+                <?php echo $fila['existencias'] ?>
+            </h6>
+            <?php if ($flag) { ?>
                 <div class="seleccion_productos">
                     <button class="seleccion_boton" type="submit" value="<?php echo $i ?>" name="Resta">-</button>
                     <p id="ProductoCarro<?php echo $i ?>">0</p>
-                    <button class="seleccion_boton" type="submit" value="<?php echo $i ?>" name="Suma">+</button>       
+                    <button class="seleccion_boton" type="submit" value="<?php echo $i ?>" name="Suma">+</button>
+                    
                 </div>
                 <div>
-                <form method="post" action="productos.php">
-                    <input style="display:none;" class="id" type="int" name="id" value="<?php echo $fila['idProducto'] ?>">
-                    <input style="display:none;" type="int" name="cantidad" id="cantidad<?php echo $i ?>" value="0">
-                    <button class="seleccion_agregar" type="submit" value="<?php echo $i ?>" name="agregar">Agregar</button>       
-                </form>                
+                    <form method="post" action="productos.php">
+                        <!-- <i class="fa-solid fa-cart-shopping fa-bounce fa-2xl" style="color: #ff3e9e;"></i> -->
+                        <input style="display:none;" class="id" type="int" name="id" value="<?php echo $fila['idProducto'] ?>">
+                        <input style="display:none;" type="int" name="cantidad" id="cantidad<?php echo $i ?>" value="0">
+
+
+                        <div class="alinear">
+
+
+                            <button class="button" type="submit" value="<?php echo $i ?>" name="agregar">
+                                <span class="button__text">Agregar</span>
+                                <span class="button__icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24"
+                                        stroke-width="2" stroke-linejoin="round" stroke-linecap="round" stroke="currentColor"
+                                        height="24" fill="none" class="svg">
+                                        <line y2="19" y1="5" x2="12" x1="12"></line>
+                                        <line y2="12" y1="12" x2="19" x1="5"></line>
+                                    </svg></span>
+                            </button>
+                        </div>
+
+
+                    </form>
                 </div>
-                <?php } ?>
-                <?php $i = $i+1 ?>
-            </div>
-        <?php } 
-    }
+            <?php } ?>
+            <?php $i = $i + 1 ?>
+        </div>
+    <?php }
+}
 
-    $servidor='localhost';
-    $cuenta='root';
-    $password='';
-    $bd='Store';
-    
-    //conexion a la base de datos
-    $conexion = new mysqli($servidor,$cuenta,$password,$bd);
+$servidor = 'localhost';
+$cuenta = 'root';
+$password = '';
+$bd = 'store';
 
-    if ($conexion->connect_errno){
-        die('Error en la conexion');
-    }
+//conexion a la base de datos
+$conexion = new mysqli($servidor, $cuenta, $password, $bd);
 
-    if(isset($_POST['agregar'])){
-        unset($_POST['agregar']);
-        $User = $_SESSION['usuario'];
-        $idPro = $_POST['id'];
-        $cantidad = $_POST['cantidad'];
-        if($cantidad != 0){
-            $sql= "INSERT INTO Carrito VALUES('$User', '$idPro', '$cantidad');";
-            $resultado = $conexion -> query($sql); 
-        }
-        header("Location: productos.php");
+if ($conexion->connect_errno) {
+    die('Error en la conexion');
+}
+
+if (isset($_POST['agregar'])) {
+    unset($_POST['agregar']);
+    $User = $_SESSION['usuario'];
+    $idPro = $_POST['id'];
+    $cantidad = $_POST['cantidad'];
+    if ($cantidad != 0) {
+        $sql = "INSERT INTO Carrito VALUES('$User', '$idPro', '$cantidad');";
+        $resultado = $conexion->query($sql);
     }
+    header("Location: productos.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -125,11 +159,11 @@ if ($_SESSION['usuario']){?>
 
     <header>
         <?php
-            include "menu.php";
+        include "menu.php";
         ?>
         <script>
-        document.getElementsByClassName("animate__animated animate__fadeInDown")[0].innerHTML = "— Productos Candy Craze —";
-        document.getElementsByClassName("animate__animated animate__fadeInUp")[0].innerHTML = "Disfruta de nuestra gran variedad de dulces";
+            document.getElementsByClassName("animate__animated animate__fadeInDown")[0].innerHTML = "— Productos Candy Craze —";
+            document.getElementsByClassName("animate__animated animate__fadeInUp")[0].innerHTML = "Disfruta de nuestra gran variedad de dulces";
         </script>
 
         <div class="wave" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
@@ -149,7 +183,7 @@ if ($_SESSION['usuario']){?>
                 <span class="name">Todos</span>
             </label>
             <label class="radio">
-                <input type="radio" name="categoria" value="0" >
+                <input type="radio" name="categoria" value="0">
                 <span class=" name">México</span>
             </label>
             <label class="radio">
@@ -166,7 +200,7 @@ if ($_SESSION['usuario']){?>
 
     <div id="Productos_div">
         <div class="Div_Productos">
-        <?php datos($conexion, $categorias, 3) ?>
+            <?php datos($conexion, $categorias, 3) ?>
         </div>
     </div>
 
@@ -177,29 +211,29 @@ if ($_SESSION['usuario']){?>
         <!-- Seccion de productos -->
         <!-- <div class="acomodo2"> -->
 
-            <!-- Diseño de la card del producto -->
-            <!-- <div class="card">
+        <!-- Diseño de la card del producto -->
+        <!-- <div class="card">
                 <div class="card-img">
                     <div class="img"> -->
-                        <!-- Aqui va la imagen -->
-                    <!-- </div>
+        <!-- Aqui va la imagen -->
+        <!-- </div>
                 </div> -->
 
-                <!-- Aqui va el titulo del producto -->
-                <!-- <div class="card-title">Product title</div> -->
+        <!-- Aqui va el titulo del producto -->
+        <!-- <div class="card-title">Product title</div> -->
 
-                <!-- Aqui va la descripcion -->
-                <!-- <div class="card-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis magni nobis
+        <!-- Aqui va la descripcion -->
+        <!-- <div class="card-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis magni nobis
                     voluptate temporibus, laboriosam, eaque quis laborum labore excepturi ratione, totam repellendus.
                     Quibusdam, id perferendis illo harum doloribus magnam repellat?</div>
                 <hr class="card-divider">
                 <div class="card-footer"> -->
 
-                    <!-- Aqui va el precio -->
-                    <!-- <div class="card-price"><span>$</span> 123.45</div> -->
+        <!-- Aqui va el precio -->
+        <!-- <div class="card-price"><span>$</span> 123.45</div> -->
 
-                    <!-- Todo esto es el apartado del diseño del boton del acrrito, entonces aqui iria la funcion del carrito -->
-                    <!-- <button class="card-btn">
+        <!-- Todo esto es el apartado del diseño del boton del acrrito, entonces aqui iria la funcion del carrito -->
+        <!-- <button class="card-btn">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                             <path
                                 d="m397.78 316h-205.13a15 15 0 0 1 -14.65-11.67l-34.54-150.48a15 15 0 0 1 14.62-18.36h274.27a15 15 0 0 1 14.65 18.36l-34.6 150.48a15 15 0 0 1 -14.62 11.67zm-193.19-30h181.25l27.67-120.48h-236.6z">
