750530f2be8debcd10a98fa76ec940d47ac28570
NadiaAcostaM <151763242+NadiaAcostaM@users.noreply.github.com>
2023-12-08 00:49:23 -0600

    Cambios Nadia
    - Se agregó el cupón a la página principal
    - En login.php se agregó un link de animación

--- index.php
+++ index.php
@@ -44,6 +44,7 @@ if (isset($_SESSION['usuario'])){
 
     <title>Inicio | Candy Craze</title>
     <link rel="stylesheet" href="css/estilospagp.css">
+    <link rel="stylesheet" href="css/estiloCupon.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
@@ -418,6 +419,33 @@ if (isset($_SESSION['usuario'])){
                 </form>
             </div>
         </section>
+        <button class="Btn" id="openCardBtn">
+            <svg class="logoIcon" height="2.5em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 6C6.89543 6 6 6.89543 6 8C6 9.10457 6.89543 10 8 10C9.10457 10 10 9.10457 10 8C10 6.89543 9.10457 6 8 6ZM4 8C4 5.79086 5.79086 4 8 4C10.2091 4 12 5.79086 12 8C12 10.2091 10.2091 12 8 12C5.79086 12 4 10.2091 4 8ZM17 15C15.8954 15 15 15.8954 15 17C15 18.1046 15.8954 19 17 19C18.1046 19 19 18.1046 19 17C19 15.8954 18.1046 15 17 15ZM13 17C13 14.7909 14.7909 13 17 13C19.2091 13 21 14.7909 21 17C21 19.2091 19.2091 21 17 21C14.7909 21 13 19.2091 13 17ZM19.7071 6.70711C20.0976 6.31658 20.0976 5.68342 19.7071 5.29289C19.3166 4.90237 18.6834 4.90237 18.2929 5.29289L5.29289 18.2929C4.90237 18.6834 4.90237 19.3166 5.29289 19.7071C5.68342 20.0976 6.31658 20.0976 6.70711 19.7071L19.7071 6.70711Z"></path></svg>
+            <span class="tooltip"><b>Cupón</b></span>
+        </button>
+
+        <div class="cardC cardC-5" id="myCard" style="position:fixed;top:20px;right:120px;z-index:1000;">
+            <p class="dismiss" type="button"><i class="fa-solid fa-xmark" style="color: #ffffff;"></i></p>
+            <br>
+            <img src="imagenes/cuponAñoNuevo.png" alt="" width=450px;>
+        </div>
+
+        <script>
+            document.addEventListener('DOMContentLoaded', function () {
+                const openCardBtn = document.getElementById('openCardBtn');
+                const myCard = document.getElementById('myCard');
+                const dismissBtn = myCard.querySelector('.dismiss');
+
+                openCardBtn.addEventListener('click', function () {
+                    // Solo mostrar la tarjeta al hacer clic en el botón
+                    myCard.style.display = 'block';
+                });
+
+                dismissBtn.addEventListener('click', function () {
+                    myCard.style.display = 'none';
+                });
+            });
+        </script>
     </main>
 
     <?php
