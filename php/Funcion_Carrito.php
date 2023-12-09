64e3029063fa7777ee1d442dd24708ce22cfca6a
DianaGabiFlores <diana.gabi.flores@gmail.com>
2023-12-08 18:33:21 -0600

    Carrito - Función de eliminar
    Borrar productos por medio de AJAX.
    Se creó un archivo php para hacer la funcionalidad.

---
+++ php/Funcion_Carrito.php
@@ -0,0 +1,16 @@
+<?php 
+    $servidor = 'localhost';
+    $cuenta = 'root';
+    $password = '';
+    $bd = 'Store';
+
+    //conexion a la base de datos
+    $conexion = new mysqli($servidor, $cuenta, $password, $bd);
+
+    if($conexion->connect_errno) {
+        die('Error en la conexion');
+    }
+
+    $sql = $_GET['sql'];
+    $result = $conexion->query($sql);
+?>
